0f55decfb7ccf95598b5716569fd52c5bb387a4d
bhagat-abhishek <inst.abhishek@gmail.com>
2023-11-20 04:21:04 +0530

    Fullpage screenshot

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://google.com');
-  await page.screenshot({path: 'example.png'});
-  await browser.close();
-})();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://techibaba.com');
+    await page.screenshot({path: 'full.png', fullPage: true});
+    await browser.close();
+  })();
